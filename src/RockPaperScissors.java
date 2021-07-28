5f7d53f5451ead225555cf5b61027cde07f2248c
jaykye <58639631+jaykye@users.noreply.github.com>
2021-07-28 11:51:25 -0400

    Reflected code review. Made variable names clearer, added more explanation in win/loss logic.

--- src/RockPaperScissors.java
+++ src/RockPaperScissors.java
@@ -6,41 +6,41 @@ public class RockPaperScissors {
         boolean wantToPlay = true;
         Scanner inputScanner = new Scanner(System.in);
         Random rSeed = new Random();
-        int nRounds = 0; // 1 to 10, inclusive.
-        int userRps = 0;  // 1. Rock, 2. Paper, 3. Scissors
-        int computerRps = 0;  // 1. Rock, 2. Paper, 3. Scissors
+        int numOfRounds = 0; // 1 to 10, inclusive.
+        int userSelection = 0;  // 1. Rock, 2. Paper, 3. Scissors
+        int computerSelection = 0;  // 1. Rock, 2. Paper, 3. Scissors
         String[] rpsArray = {"Rock", "Paper", "Scissors"};
         String input;
-        int nUserWin;
-        int nUserTie;
-        int nUserLoss;
+        int userWinCount;
+        int userTieCount;
+        int userLossCount;
         boolean validAnswer;
 
         while (wantToPlay) {
             while (true){
                 System.out.println("How many rounds do you want to play?");
-                nRounds = Integer.parseInt(inputScanner.nextLine());
-                if (1 <= nRounds && nRounds <=10){
+                numOfRounds = Integer.parseInt(inputScanner.nextLine());
+                if (1 <= numOfRounds && numOfRounds <=10){
                     break;
                 }
                 System.out.println("Round must be between 1 to 10.");
 
             }
-            System.out.println("Playing " + nRounds + " round(s).");
+            System.out.println("Playing " + numOfRounds + " round(s).");
 
-            nUserWin = 0;
-            nUserTie = 0;
-            nUserLoss = 0;
+            userWinCount = 0;
+            userTieCount = 0;
+            userLossCount = 0;
             // Loop as many times as number of rounds.
-            for (int i=0; i<nRounds; i++){
+            for (int i=0; i<numOfRounds; i++){
 
                 // User selection and Computer selection.
                 validAnswer = false;
                 while (!validAnswer) {
                     System.out.println("Type in integer: 1. Rock, 2. Paper, 3. Scissors");
                     try{
-                        userRps = Integer.parseInt(inputScanner.nextLine());
-                        if (0< userRps && userRps <=3){
+                        userSelection = Integer.parseInt(inputScanner.nextLine());
+                        if (0< userSelection && userSelection <=3){
                             validAnswer = true;
                         }
                         else{
@@ -51,43 +51,45 @@ public class RockPaperScissors {
                         System.out.println("Enter integer value.");
                     }
                 }
-                System.out.println("User selected: " + rpsArray[userRps-1]);
-                computerRps = 1 + rSeed.nextInt(3);
-                System.out.println("Computer selected: " + rpsArray[computerRps-1]);
+                System.out.println("User selected: " + rpsArray[userSelection-1]);
+                computerSelection = 1 + rSeed.nextInt(3);
+                System.out.println("Computer selected: " + rpsArray[computerSelection-1]);
 
                 // win/loss logic
-                switch (reportResults(userRps, computerRps)){
+                switch (reportResults(userSelection, computerSelection)){
                     case 1:
-                        nUserWin +=1;
+                        userWinCount +=1;
                         System.out.println("You won this round.");
                         break;
                     case -1:
-                        nUserLoss +=1;
+                        userLossCount +=1;
                         System.out.println("You lost this round.");
                         break;
                     case 0:
-                        nUserTie +=1;
+                        userTieCount +=1;
                         System.out.println("Tied this round.");
                         break;
                 }
             }
 
             // Report results
-            System.out.println("Number of wins: " + nUserWin);
-            System.out.println("Number of ties: " + nUserTie);
-            System.out.println("Number of losses: " + nUserLoss);
-            if (nUserWin > nUserLoss){
+            System.out.println("Number of wins: " + userWinCount);
+            System.out.println("Number of ties: " + userTieCount);
+            System.out.println("Number of losses: " + userLossCount);
+            if (userWinCount > userLossCount){
                 System.out.println("You have won this game!");
             }
-            else if (nUserWin < nUserLoss){
+            else if (userWinCount < userLossCount){
                 System.out.println("You have lost this game!");
             }
             else{
                 System.out.println("Tied Game!");
 
             }
+
             // Ask if want to play more.
             validAnswer = false;
+
             while (!validAnswer){
                 System.out.println("Do you want to play more? Yes/No");
                 input = inputScanner.nextLine();
@@ -116,7 +118,12 @@ public class RockPaperScissors {
         /*
         Win / Loss logic
         if user wins, this method returns 1, ties 0, loss -1
+
+        The integer equivalent of rock paper scissors have the following pattern:
+        If user value is bigger than the computer value by 1, user wins.
+        One edge case is when user selects 1, in this case computer cannot select 0. -> use -2
         */
+
         if (userVal - compVal == 1 || userVal - compVal == -2){
             return 1;
         }
